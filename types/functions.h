761ba81eab47b416fa67414df2ca604f3bf8ca06
Helder Nicollas Leão Peixoto <nicollashelder@gmail.com>
2025-10-21 20:35:01 -0300

    feat: add letters array

--- types/functions.h
+++ types/functions.h
@@ -11,6 +11,13 @@ typedef struct {
     int d;
 } SecretKey;
 
+const char LETTERS[27] = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G',
+    'H', 'I', 'J', 'K', 'L', 'M', 'N',
+    'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+    'V', 'W', 'X', 'Y', 'Z', ' '
+};
+
 PublicKey generatePublicKey(int p, int q, int e);
 SecretKey generateSecretKey(int p, int q, int e);
 
